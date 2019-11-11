cfb797fb8aa4cfcfafc9e1d81b7d6b82cd72dcbf
Arnsdorf <38246817+Arnsdorf@users.noreply.github.com>
2019-11-11 19:12:03 +0100

    sigurd
    jnlm

---
+++ GYS/Assets/FlashLight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    private Light myLight;
+
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
+    }
+}
